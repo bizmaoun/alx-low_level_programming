1ba5abaae0ce6b03f24574db855fc88446db777d
bizmaoun <bizmaounl2007@gmail.com>
2023-03-30 20:08:10 -0700

    commit 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,13 +7,11 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, j, tmp, mid;
+	int i, j, tmp;
 
 	j = n - 1;
 
-	mid = n/2;
-
-	for (i = 0; i < mid; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		tmp = a[i];
 		a[i] = a[j];
