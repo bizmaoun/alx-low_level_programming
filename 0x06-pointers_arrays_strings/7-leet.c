e2adb7bb940b665e617e4628099f79d06dbc03d1
bizmaoun <bizmaounl2007@gmail.com>
2023-03-31 19:20:23 -0700

    commit 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string in 1337
+ * @s: string to be encoded
+ *
+ * Return: the resulting string;
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char *let = "aAeEoOtTlL";
+	char *nb = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == let[j])
+			{
+				s[i] = nb[j];
+			}
+		}
+	}
+
+	return (s);
+}
